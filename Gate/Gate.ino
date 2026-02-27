c50764a67162e86bac3ae7dae6bb20a777b8aec7
CamilaColussi-KSA <c.colussi@kingston.ac.uk>
2026-02-27 08:32:07 +0000

    Update Gate.ino

--- Gate/Gate.ino
+++ Gate/Gate.ino
@@ -14,8 +14,12 @@ Sparkfun Sound Detector
 void setup() {
   Serial.begin( 9600 );
   pinMode( gatePin, INPUT );
+
+  Serial.println("Initialized");
+  delay(2000);
 }
 
 void loop() {
-  Serial.println( digitalRead( gatePin ) );
+  int gate = digitalRead(gatePin);
+  Serial.println(gate);
 }
