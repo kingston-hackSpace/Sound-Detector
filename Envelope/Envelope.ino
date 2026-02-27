2d7eb518adc840e6beea4570392feca5acf96e1c
CamilaColussi-KSA <c.colussi@kingston.ac.uk>
2026-02-27 08:31:36 +0000

    Update Envelope.ino

--- Envelope/Envelope.ino
+++ Envelope/Envelope.ino
@@ -13,9 +13,13 @@ Sparkfun Sound Detector
 
 void setup() {
   Serial.begin( 9600 );
-  pinMode( envelopePin, INPUT );
+  pinMode(envelopePin,INPUT);
+
+  Serial.println("Initialized");
+  delay(2000);
 }
 
 void loop() {
-  Serial.println( analogRead( envelopePin ) );
+  int envelope = analogRead(envelopePin);
+  Serial.println(envelop);
 }
