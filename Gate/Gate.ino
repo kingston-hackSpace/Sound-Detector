2d21cbdb9cca6d4b618b5d8ef8837f59ac014507
CamilaColussi-KSA <c.colussi@kingston.ac.uk>
2026-02-27 09:35:19 +0000

    Add files via upload

--- Gate/Gate.ino
+++ Gate/Gate.ino
@@ -1,25 +1,34 @@
 /*
 SparkFun Sound Detector
 GATE MODE
-Detecting sound presence
+Detecting sound and visualizing with an LED
 
 Hardware:
 Arduino UNO
 Sparkfun Sound Detector
-
+LED + 220ohms resistor
 */
 
 #define gatePin 2 //digital pin
+#define LED_pin 9
 
 void setup() {
   Serial.begin( 9600 );
-  pinMode( gatePin, INPUT );
+  pinMode (gatePin, INPUT);
+  pinMode (LED_pin, OUTPUT);
 
   Serial.println("Initialized");
   delay(2000);
+
 }
 
 void loop() {
   int gate = digitalRead(gatePin);
   Serial.println(gate);
+
+  if (gate == HIGH){
+    digitalWrite(LED_pin, HIGH);
+  } else {
+    digitalWrite(LED_pin, LOW);
+  }
 }
